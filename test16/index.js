172e10ea3631169096d2ebbd7377854108c85fb6
xieruijie2654 <2654767808@qq.com>
2021-10-07 21:53:23 +0800

    demo

--- test16/index.js
+++ test16/index.js
@@ -1,8 +1,7 @@
-import Proton from proton - engine;
 const proton = new Proton();
 const emitter = new Proton.Emitter();
-const canvasEl = document.getElementById('canvas');
-const context = canvasEl.getContext('2d');
+const canvasEl = document.getElementById("canvas");
+const context = canvasEl.getContext("2d");
 
 canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
@@ -17,11 +16,11 @@ emitter.rate = new Proton.Rate(Proton.getSpan(20, 40));
 emitter.addInitialize(new Proton.Radius(0.5, 2));
 emitter.addInitialize(new Proton.Life(5));
 emitter.addInitialize(
-    new Proton.Velocity(new Proton.Span(1, 2), new Proton.Span(0, 360), 'polar')
+    new Proton.Velocity(new Proton.Span(1, 2), new Proton.Span(0, 360), "polar")
 );
 
 emitter.addBehaviour(new Proton.Gravity(1));
-emitter.addBehaviour(new Proton.Color('random', '#ffffff'));
+emitter.addBehaviour(new Proton.Color("random", "#ffffff"));
 emitter.addBehaviour(new Proton.Scale(3, 0));
 emitter.addBehaviour(new Proton.Alpha(1, 0));
 
@@ -30,23 +29,23 @@ proton.addEmitter(emitter);
 const renderer = new Proton.CanvasRenderer(canvasEl);
 
 renderer.onProtonUpdate = () => {
-    context.fillStyle = "rgba(0,0,0,0.1)"
-    context.fillRect(0, 0, canvasEl.width, canvasEl.height)
-}
+    context.fillStyle = "rgba(0,0,0,0.1)";
+    context.fillRect(0, 0, canvasEl.width, canvasEl.height);
+};
 
 proton.addRenderer(renderer);
 
 RAFManager.add(() => {
     proton.update();
-}, 60)
+}, 60);
 
-canvasEl, onclick = handleClick;
+canvasEl, (onclick = handleClick);
 
 function handleClick(event) {
     emitter.p.x = event.clientX;
     emitter.p.y = event.clientY;
-    emitter.emit('once');
+    emitter.emit("once");
     setTimeout(() => {
-        emitter.emit('once');
-    })
+        emitter.emit("once");
+    });
 }
